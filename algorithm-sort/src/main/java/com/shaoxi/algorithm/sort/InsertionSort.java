8534dfc97d28bbedf838d506e5e2d154197eabbd
shaoxi.ycw <shaoxi.ycw@taobao.com>
2019-01-19 22:11:09 +0800

    test

---
+++ algorithm-sort/src/main/java/com/shaoxi/algorithm/sort/InsertionSort.java
@@ -0,0 +1,17 @@
+package com.shaoxi.algorithm.sort;
+
+/**
+ * @author shaoxi.ycw
+ * @since 2019-01-19
+ */
+public class InsertionSort {
+
+    /**
+     * 插入排序
+     *
+     * @param a
+     */
+    public void sort(int[] a){
+
+    }
+}
---
+++ algorithm-sort/src/test/java/com/shaoxi/algorithm/sort/InsertionSortTest.java
@@ -0,0 +1,13 @@
+package com.shaoxi.algorithm.sort;
+
+/**
+ * @author shaoxi.ycw
+ * @since 2019-01-19
+ */
+public class InsertionSortTest {
+
+    
+    public void testSort(){
+
+    }
+}
